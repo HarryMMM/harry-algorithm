1126329930e6bebcf46b7149e224c38dee3091a4
harry <hhr_harry@qq.com>
2019-09-13 16:44:48 +0800

    1. Add the solution of the second problem 《Add Two Numbers》 in leetCode。

--- src/main/java/com/harryai/algorithm/algorithm/leetcode/No1TwoSum.java
+++ src/main/java/com/harryai/algorithm/algorithm/leetcode/No1TwoSum.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * @author Harry
  * TwoSum
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * <p>
@@ -16,21 +17,21 @@ import java.util.Map;
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
  */
-public class No_1_TwoSum {
+public class No1TwoSum {
 
     public static void main(String[] args) {
-        int[] ints = new No_1_TwoSum().twoSum(new int[]{3, 2, 4}, 6);
+        int[] ints = new No1TwoSum().twoSum(new int[]{3, 2, 4}, 6);
     }
 
     public int[] twoSum(int[] nums, int target) {
         int[] index = new int[2];
         int len = nums.length;
         for (int i = 0; i < len; i++) {
-            for (int j = i + 1; j < len; j++) { // j要等于i+1,因为不和自己相加。
+            for (int j = i + 1; j < len; j++) {
                 if (nums[i] + nums[j] == target) {
                     index[0] = i;
                     index[1] = j;
-                    i = len; // 使外层循环也结束
+                    i = len;
                     break;
                 }
             }
@@ -38,9 +39,11 @@ public class No_1_TwoSum {
         return index;
     }
 
-    // 此方法借助map，来实现。体现了解决问题加中间层的方法。值得学习
+    /**
+     * 此方法借助map，来实现。体现了解决问题加中间层的方法。值得学习
+     */
     public int[] twoSum2(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
         // 将数值以key: number  value:index 的方式存入map
         for (int i = 0; i < nums.length; map.put(nums[i], i++))
             // 如果遍历到目标值与当前元素的差值在map中，则将差值的下标从map中去取出，和当前元素的下标一起返回。
