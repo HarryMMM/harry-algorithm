9bcc9299dd0a80dadda6deb4c809abec4bdbc0e3
harry <hhr_harry@qq.com>
2019-12-15 21:13:03 +0800

    1. Add other bubble sorting methods to achieve

--- src/main/java/com/harryai/algorithm/algorithm/selfstudy/sort/QuickSort.java
+++ src/main/java/com/harryai/algorithm/algorithm/selfstudy/sort/QuickSort.java
@@ -62,15 +62,19 @@ public class QuickSort {
             int i = left, j = right;
             T pivot = data.get(left);
             while (i < j) {
-                while (i < j && data.get(j).compareTo(pivot) >= 0) // 从右向左找第一个小于x的数
+                // 从右向左找第一个小于x的数
+                while (i < j && data.get(j).compareTo(pivot) >= 0) {
                     j--;
+                }
                 if (i < j)
                     data.set(i++, data.get(j));
-
-                while (i < j && data.get(i).compareTo(pivot) <= 0) // 从左向右找第一个大于等于x的数
+               // 从左向右找第一个大于等于x的数
+                while (i < j && data.get(i).compareTo(pivot) <= 0) {
                     i++;
-                if (i < j)
+                }
+                if (i < j) {
                     data.set(j--, data.get(i));
+                }
             }
             data.set(i, pivot);
             sort(data, left, i - 1); // 递归调用
@@ -78,6 +82,35 @@ public class QuickSort {
         }
     }
 
+    /**
+     * 交换奇数偶数，使奇数分布在表头，使偶数分布在表尾
+     * 时间复杂度为0(N),空间复杂度为0(1)。
+     */
+    public static void swapOddvenNumbers(int []nums){
+        int i=0;
+        int j=nums.length-1;
+        while (i<j){
+            // 当i指示的位偶数，j指示的为奇数，进行交换
+            if(nums[i]%2==0&&nums[j]%2==1){
+                int tmp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=tmp;
+                // 否则，如果i指示的为偶数，并且j指示的也为偶数
+                // i不动，j向前移动
+            }else if(nums[i]%2==0&&nums[j]%2==0) {
+                j--;
+                // 否则，如果i指示的为奇数，并且j指示的也为奇数
+                // j不动，i向后移动
+            }else if(nums[i]%2==1&&nums[j]%2==1){
+                i++;
+                // 否则，如果i指示的为奇数，并且j指示的为偶数
+                // i和j都移动
+            }else {
+                i++;
+                j--;
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
--- src/test/java/com/harryai/algorithm/AlgorithmApplicationTests.java
+++ src/test/java/com/harryai/algorithm/AlgorithmApplicationTests.java
@@ -4,13 +4,17 @@ import com.harryai.algorithm.Util.DataPrepareUtil;
 import com.harryai.algorithm.Util.LogUtil;
 import com.harryai.algorithm.Util.RandomUtil;
 import com.harryai.algorithm.Util.TreeNodePrepareUtil;
+import com.harryai.algorithm.algorithm.selfstudy.sort.MergeSort;
 import com.harryai.algorithm.algorithm.selfstudy.sort.QuickSort;
 import com.harryai.algorithm.mode.algorithm.TreeNode;
 import org.apache.commons.lang3.SerializationUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
@@ -48,6 +52,66 @@ public class AlgorithmApplicationTests {
         long quickSortEnd = LogUtil.logCost("quick sort", cloneEnd);
         Arrays.sort(timClone);
         long timSortEnd = LogUtil.logCost("tim sort", quickSortEnd);
+    }
+
+    @Test
+    public void test() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(2);
+        linkedList.add(4);
+        linkedList.add(6);
+        linkedList.add(8);
+        linkedList.add(8);
+        linkedList.add(10);
+        linkedList.add(11);
+        System.out.println(linkedList);
+
+        LinkedList<Integer> linkedList2 = new LinkedList<>();
+        linkedList2.add(1);
+        linkedList2.add(4);
+        linkedList2.add(6);
+        linkedList2.add(6);
+        linkedList2.add(8);
+        linkedList2.add(9);
+        linkedList2.add(10);
+        System.out.println(linkedList2);
+
+        LinkedList<Integer> linkedList3 = new LinkedList<>();
+        linkedList3.add(4);
+        linkedList3.add(5);
+        linkedList3.add(6);
+        linkedList3.add(7);
+        linkedList3.add(8);
+        linkedList3.add(9);
+        linkedList3.add(10);
+
+        System.out.println(linkedList3);
+
+
+        MergeSort.deleteOwnedByAll(linkedList, linkedList2, linkedList3);
+        System.out.println(linkedList);
+
+
+    }
+
+    @Test
+    public void test33() {
+
+//        int[] a = {20, 22, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17};
+//        QuickSort.swapOddvenNumbers(a);
+//        System.out.println(ArrayUtils.toString(a));
+//
+        Stream.of("Apsara", "2019", "9.25 - 9.27", "Digital Economy")
+                .map(x -> 57 == x.charAt(0) ? "Here is the" :
+                        50 == x.charAt(0) ? x.concat(":") : x)
+                .reduce((x, y) -> x + " " + y)
+                .ifPresent(System.out::println);
+
+        int []a={1,2,3,4};
+        String []b={"I","am","harry"};ArrayBlockingQueue
+        int reduce = Arrays.stream(a).reduce(1, Integer::sum);
+        System.out.println(reduce);
+
     }
 
 }
