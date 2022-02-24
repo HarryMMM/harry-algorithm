e79a732e26d79f000222b64eaaad1e3588300516
haorui.hao <haorui.hao@accenture.com>
2022-02-24 19:59:53 +0800

    feat(leetcode):resolve 《Longest Substring Without Repeating Characters》 by another method

--- src/main/java/com/harryai/algorithm/leetcode/editor/en/P3LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/harryai/algorithm/leetcode/editor/en/P3LongestSubstringWithoutRepeatingCharacters.java
@@ -39,6 +39,9 @@
 
 package com.harryai.algorithm.leetcode.editor.en;
 
+import java.util.HashMap;
+import java.util.Map;
+
 //java:Longest Substring Without Repeating Characters
 public class P3LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
@@ -50,25 +53,19 @@ public class P3LongestSubstringWithoutRepeatingCharacters {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int lengthOfLongestSubstring(String s) {
-            // #1 定义两个游标，left和right.[left,right)为一个字符串S。一个记录字符串S是否用重复的数组int[] chars，ASCII有128个字符串，所以定义128长度
-            // #2 使用right向字符串的右侧进行探索。如果没有重复的就一直探索。同时在chars中每个字符串出现的次数
-            // #3 当发现重复字符S(right)时候，right停止探索。此时一同向右移动left游标，并将游标所处的字符的出现次数减一。
-            // #4 left一直移动直到遇到左侧的重复字符S(left)的后一位，此时记录S的长度（如果比上一个结果大）。
-            // #5 right继续探索，重复2-5，直到字符串检查完毕。
-            int[] chars = new int[128];
-            int left = 0;
-            int right = 0;
-            int res = 0;
-            while (right < s.length()) {
-                char r = s.charAt(right);
-                chars[r]++;
-                while (chars[r] > 1) {
-                    char l = s.charAt(left);
-                    chars[l]--;
-                    left++;
+            // 定义左右游标l和r, 使用Map存储r滑动过的字符串位置
+            // r在滑动过程中，如果发现当前字符串cur与之前的重复。则将左游标移动到最左边重复字符串的下一个位置
+            // 然后记录遇到重复前l和f之间的字符串个数（原有的和现有的哪个长取哪个）。
+            // 如果没有遇到重复的，则滑动一个字符，记录一个长度
+            int res = 0, n = s.length();
+            Map<Character, Integer> map = new HashMap<>();
+            for (int l = 0, r = 0; r < n; r++) {
+                char cur = s.charAt(r);
+                if (map.containsKey(cur)) {
+                    l = Math.max(map.get(cur) + 1, l);
                 }
-                res = Math.max(res, right - left + 1);
-                right++;
+                res = Math.max(res, r - l + 1);
+                map.put(cur, r);
             }
             return res;
         }
