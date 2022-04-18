0ec4f9278eac298a6a19f1cc9bf237b55c1b3ddc
haorui.hao <haorui.hao@accenture.com>
2022-04-18 13:51:17 +0800

    feat(practicing): Merge Sort

--- src/main/java/com/harryai/algorithm/practicing/sort/MergeSort.java
+++ src/main/java/com/harryai/algorithm/practicing/sort/MergeSort.java
@@ -23,10 +23,10 @@ public final class MergeSort {
 
     public static void main(String[] args) {
 
+//        Integer[] arr1 = {3,2};
         Integer[] arr1 = ArrayUtils.clone(IntegerData.ELE5_ARRAY_FIRST_2_SECOND_PLACE);
-        ;
         System.out.println(Arrays.toString(arr1));
-        sort(arr1);
+        sort2(arr1);
         System.out.println(Arrays.toString(arr1));
 
     }
@@ -57,5 +57,43 @@ public final class MergeSort {
         T[] sortRight = sort1(sr);
         return ArrayUtil.mergeSortedArrayAsc(sortLeft, sortRight);
 
+    }
+
+    private static void sort2(Integer[] arr) {
+        // 少于两个，不用排序。
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        doSort(arr, 0, arr.length - 1);
+    }
+
+    private static void doSort(Integer[] arr, int left, int right) {
+        if (right - left + 1 <= 1) {
+            return;
+        }
+        int middle = ((right - left) >> 2) + left;
+        doSort(arr, left, middle);
+        doSort(arr, middle + 1, right);
+        Integer[] tmp = new Integer[right - left + 1];
+
+        int p1 = left, p2 = middle+1;
+        int index = 0;
+        while (p1 <= middle && p2 <= right) {
+            if (arr[p1] >= arr[p2]) {
+                tmp[index++] = arr[p2++];
+            } else {
+                tmp[index++] = arr[p1++];
+            }
+        }
+        while (p1 <= middle) {
+            tmp[index++] = arr[p1++];
+        }
+        while (p2 <= right) {
+            tmp[index++] = arr[p2++];
+        }
+        int in = 0;
+        while (left <= right) {
+            arr[left++] = tmp[in++];
+        }
     }
 }
