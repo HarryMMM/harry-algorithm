391b7e2bee05edbda977e30e69346d5fab84f9f4
haorui.hao <haorui.hao@accenture.com>
2023-02-27 17:17:29 +0800

    P104 recursive solution

---
+++ src/main/java/com/harryai/algorithm/leetcode/editor/en/P104MaximumDepthOfBinaryTree.java
@@ -0,0 +1,91 @@
+//Given the root of a binary tree, return its maximum depth. 
+//
+// A binary tree's maximum depth is the number of nodes along the longest path 
+//from the root node down to the farthest leaf node. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [3,9,20,null,null,15,7]
+//Output: 3
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = [1,null,2]
+//Output: 2
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [0, 10⁴]. 
+// -100 <= Node.val <= 100 
+// 
+// Related Topics Tree Depth-First Search Breadth-First Search Binary Tree 👍 10
+//232 👎 162
+
+package com.harryai.algorithm.leetcode.editor.en;
+
+//java:Maximum Depth of Binary Tree
+public class P104MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new P104MaximumDepthOfBinaryTree().new Solution();
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            return calc(root, 0);
+        }
+
+        public int calc(TreeNode root, int curCount) {
+            if (root == null) {
+                return curCount;
+            }
+            int l = calc(root.left, curCount + 1);
+            int r = calc(root.right, curCount + 1);
+            return Math.max(l, r);
+        }
+    }
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
