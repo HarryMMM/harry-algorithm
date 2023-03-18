33e12f3afc6bbb0c765acbe4d9d3f389b37d5a18
haorui.hao <haorui.hao@accenture.com>
2023-03-18 23:48:48 +0800

    feature(P145) second solution: loop

--- src/main/java/com/harryai/algorithm/leetcode/editor/en/P145BinaryTreePostorderTraversal.java
+++ src/main/java/com/harryai/algorithm/leetcode/editor/en/P145BinaryTreePostorderTraversal.java
@@ -41,6 +41,8 @@ package com.harryai.algorithm.leetcode.editor.en;
 import com.harryai.algorithm.common.tree.model.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 //java:Binary Tree Postorder Traversal
@@ -67,21 +69,32 @@ public class P145BinaryTreePostorderTraversal{
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        postorderTraversal(root, list);
-        return list;
-    }
-
-    public void postorderTraversal(TreeNode root, List<Integer> list) {
-        if (root == null) {
-            return;
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode pre = null;
+        // 1. 如果根节不为null,点把根节点入栈,并把左子树置为根节点（优先处理左子树）
+        // 2. 继续判断根节点是为null,如果为null，从栈中取出一个节点。
+        // 3. 因为栈中的节点的要么没有左子树，要么左子树已经处理过了。
+        // 所以直接判断取出的节点是否有右子树如果取出的节点没有右子树或者又子树已经处理过了（左子树之前也处理过了，），
+        // 则将取出的节点记录到结果中。并将取出的节点置为上一个已处理过的节点，并且根节点置为null.并继续从栈中取出节点
+        // 4. 如果取出的节点有右子树，当前节点入栈。把右子树置为根节点。下一步循环继续优先处理左子树。。。重复1-4
+        //
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                if (root.right == null | root.right == pre) {
+                    list.add(root.val);
+                    pre = root;
+                    root = null;
+                } else {
+                    stack.push(root);
+                    root = root.right;
+                }
+            }
         }
-        // traverse left node
-        postorderTraversal(root.left, list);
-        // traverse right  node
-        postorderTraversal(root.right, list);
-        // traverse root node
-        list.add(root.val);
-
+        return list;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
