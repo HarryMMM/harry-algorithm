3758ae77922e938c40e8d694096f8eaed3ca36ca
haorui.hao <haorui.hao@accenture.com>
2023-03-18 11:51:01 +0800

    feature(P145) second solution: loop

---
+++ src/main/java/com/harryai/algorithm/leetcode/editor/en/P145BinaryTreePostorderTraversal.java
@@ -0,0 +1,73 @@
+//Given the root of a binary tree, return the postorder traversal of its nodes' 
+//values. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: root = [1,null,2,3]
+//Output: [3,2,1]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = [1]
+//Output: [1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of the nodes in the tree is in the range [0, 100]. 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//Follow up: Recursive solution is trivial, could you do it iteratively?
+//
+// Related Topics Stack Tree Depth-First Search Binary Tree 👍 5702 👎 168
+
+package com.harryai.algorithm.leetcode.editor.en;
+
+import com.harryai.algorithm.common.tree.model.TreeNode;
+
+import java.util.List;
+
+//java:Binary Tree Postorder Traversal
+public class P145BinaryTreePostorderTraversal{
+    public static void main(String[] args){
+        Solution solution = new P145BinaryTreePostorderTraversal().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
