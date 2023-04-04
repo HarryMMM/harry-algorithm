ed5946f818843d26043cfefc9a60a3fef65cbc32
haorui.hao <haorui.hao@accenture.com>
2023-04-04 09:09:19 +0800

    feature(P225) use one queue to implement a stack

--- src/main/java/com/harryai/algorithm/leetcode/editor/en/P225ImplementStackUsingQueues.java
+++ src/main/java/com/harryai/algorithm/leetcode/editor/en/P225ImplementStackUsingQueues.java
@@ -65,9 +65,7 @@ public class P225ImplementStackUsingQueues {
         MyStack myStack = new P225ImplementStackUsingQueues().new MyStack();
         myStack.push(1);
         myStack.push(2);
-        myStack.push(3);
-        System.out.println(myStack.pop());
-        System.out.println(myStack.pop());
+        System.out.println(myStack.top());
         System.out.println(myStack.pop());
         System.out.println(myStack.empty());
     }
@@ -79,55 +77,126 @@ public class P225ImplementStackUsingQueues {
         // peek 从队首检出
         // size 元素数量
         // isEmpty 队列是否为空
-        // master 和 salver pop时互相倒元素，每次最后一个丢弃，达到LIFO的目的
-        private Queue<Integer> queue = new LinkedList<>();
-        // 记录队尾，以供top,pop使用
-        int tail = -1;
+        private Queue<Integer> queue1 = new LinkedList<>();
 
         public MyStack() {
 
         }
 
         public void push(int x) {
-            queue.offer(x);
-            tail = x;
+            // 记录元素个数，添加新元素，然后将前n个元素重新添加到队尾，前后颠倒元素
+            int n = queue1.size();
+            queue1.offer(x);
+            for (int i = 0; i < n; i++) {
+                queue1.offer(queue1.poll());
+            }
         }
 
         // 使用弹出所有元素，使用重新添加（但不添加最后一个元素的方式）来实现pop
         public int pop() {
-            int tmpTail = tail;
-            // 记录本次pop后的队尾
-            int newTail = -1;
-            // 记录元素处理的个数
-            int eleIdx = 1;
-            int size = queue.size();
-            // 只要本次所有元素没有重新添加完成，就不停止
-            while (eleIdx <= size && !queue.isEmpty()) {
-                Integer poll = queue.poll();
-                // 最后一个元素弹出但不添加
-                if (eleIdx != size) {
-                    queue.offer(poll);
-                    newTail = poll;
-                } else {
-                    // 重置队尾元素
-                    tail = newTail;
-                }
-                // 重新添加一个元素后，索引加1
-                eleIdx++;
-            }
-            // 返回最后一个元素
-            return tmpTail;
-
+            return queue1.poll();
         }
 
         public int top() {
-            return tail;
+            return queue1.peek() ;
         }
 
         public boolean empty() {
-            return queue.isEmpty();
+            return queue1.isEmpty();
         }
     }
+//    class MyStack {
+//        // offer 入队
+//        // poll 出队
+//        // peek 从队首检出
+//        // size 元素数量
+//        // isEmpty 队列是否为空
+//        private Queue<Integer> queue1 = new LinkedList<>();
+//        private Queue<Integer> queue2 = new LinkedList<>();
+//
+//        public MyStack() {
+//
+//        }
+//
+//        public void push(int x) {
+//            // 先入栈到queue2, 再把queue1中的元素放到queue2,此时相当于前后颠倒了元素
+//            queue2.offer(x);
+//            while (!queue1.isEmpty()) {
+//                queue2.offer(queue1.poll());
+//            }
+//            Queue<Integer> tmp = queue1;
+//            queue1 = queue2;
+//            queue2 = tmp;
+//        }
+//
+//        // 使用弹出所有元素，使用重新添加（但不添加最后一个元素的方式）来实现pop
+//        public int pop() {
+//            return queue1.poll();
+//        }
+//
+//        public int top() {
+//            return queue1.peek();
+//        }
+//
+//        public boolean empty() {
+//            return queue1.isEmpty();
+//        }
+//    }
+//    class MyStack {
+//        // offer 入队
+//        // poll 出队
+//        // peek 从队首检出
+//        // size 元素数量
+//        // isEmpty 队列是否为空
+//        // master 和 salver pop时互相倒元素，每次最后一个丢弃，达到LIFO的目的
+//        private Queue<Integer> queue = new LinkedList<>();
+//        // 记录队尾，以供top,pop使用
+//        int tail = -1;
+//
+//        public MyStack() {
+//
+//        }
+//
+//        public void push(int x) {
+//            queue.offer(x);
+//            tail = x;
+//        }
+//
+//        // 使用弹出所有元素，使用重新添加（但不添加最后一个元素的方式）来实现pop
+//        public int pop() {
+//            int tmpTail = tail;
+//            // 记录本次pop后的队尾
+//            int newTail = -1;
+//            // 记录元素处理的个数
+//            int eleIdx = 1;
+//            int size = queue.size();
+//            // 只要本次所有元素没有重新添加完成，就不停止
+//            while (eleIdx <= size && !queue.isEmpty()) {
+//                Integer poll = queue.poll();
+//                // 最后一个元素弹出但不添加
+//                if (eleIdx != size) {
+//                    queue.offer(poll);
+//                    newTail = poll;
+//                } else {
+//                    // 重置队尾元素
+//                    tail = newTail;
+//                }
+//                // 重新添加一个元素后，索引加1
+//                eleIdx++;
+//            }
+//            // 返回最后一个元素
+//            return tmpTail;
+//
+//        }
+//
+//        public int top() {
+//            return tail;
+//        }
+//
+//        public boolean empty() {
+//            return queue.isEmpty();
+//        }
+//    }
 //    class MyStack {
 //        // offer 入队
 //        // poll 出队
