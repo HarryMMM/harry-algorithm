899bac6afc4b6745aeea0a49d83105c671756bc8
haorui.hao <haorui.hao@accenture.com>
2023-07-27 16:50:57 +0800

    feature(P22) solving problem 22 by solution 4

--- src/main/java/com/harryai/algorithm/leetcode/editor/cn/P22GenerateParentheses.java
+++ src/main/java/com/harryai/algorithm/leetcode/editor/cn/P22GenerateParentheses.java
@@ -28,7 +28,7 @@ import java.util.List;
 public class P22GenerateParentheses {
     public static void main(String[] args) {
         Solution solution = new P22GenerateParentheses().new Solution();
-        System.out.println(solution.generateParenthesis(3));
+        System.out.println(solution.generateParenthesis(7));
 //        System.out.println(solution.generateParenthesis(2));
 //        System.out.println(solution.generateParenthesis(3));
     }
@@ -37,43 +37,47 @@ public class P22GenerateParentheses {
 
     /**
      * topic: Generate Parentheses
-     * Solution number:3
-     * Ideas:回溯+剪枝，在生成过程中判断字符串序列是否有效。规则
-     * 1. 左括号大于0时才继续拼左括号
-     * 2. 右括号大于左括号时才拼接右括号（因为有效的括号一定是先有左括号，再有右括号）
-     * Problem boundary: 字符生成结束（字符串生成到最后一位）
-     * Problem pattern: 递归，问题和子问题
-     * time complexity: O(4N/根N) 与
+     * Solution number:4
+     * Ideas:
+     * n种括号的组合一定是1组括号和n-1组括号的组合
+     * 括号序列一定是这种格式:
+     * 设求n组括号的组合，p+q=n-1，因为起始一个括号一定为 "(", 对应的")”括号位置为 2p+1则括号序列为：
+     * "(" + p情况的括号序列 + ")" + q情况的序列
+     * 所以，罗列p和q两种情况的括号序列并和 一组 ”()“ 进行组合
+     * Problem boundary: 遍历完
+     * Problem pattern: 动态规划，递归，
+     * time complexity: O(4N/根N)
      * space complexity O(4N/根N)
      */
     class Solution {
+        ArrayList[] cache = new ArrayList[100];
 
         public List<String> generateParenthesis(int n) {
-            List<String> combinations = new ArrayList<>();
-            generateAll(new char[2 * n], 0, combinations,n,n);
-            return new ArrayList<>(combinations);
+            return generate(n);
         }
 
-        /**
-         * @param pos 当需要放置字符的位置
-         * @param result 存储结果的列表
-         */
-        public void generateAll(char[] current, int pos, List<String> result, int l, int r) {
-            // 表示生成结束，检查，如果合法则添加
-            if (pos == current.length) {
-                result.add(new String(current));
+        private List<String> generate(int n) {
+            // 如果有缓存，直接返回
+            if (cache[n] != null) {
+                return cache[n];
+            }
+            ArrayList<String> ans = new ArrayList<>();
+            if (n == 0) {
+                ans.add("");
             } else {
-                // 1.当还有左括号时，生成在当前位置放置左括号的字符串
-                if (l > 0) {
-                    current[pos] = '(';
-                    generateAll(current, pos + 1, result, l - 1, r);
-                }
-                // 2.当右括号数量大于左括号数量时,生成在当前位置放置右括号的字符串
-                if (r > l) {
-                    current[pos] = ')';
-                    generateAll(current, pos + 1, result, l, r - 1);
+                // 总共有n组括号进行组合，依次计算0,1,2 ... n 组括号的序列
+                for (int i = 0; i < n; i++) {
+                    // 遍历p种情况的括号序列
+                    for (String p : generate(i)) {
+                        // 遍历q种情况的括号序列
+                        for (String q : generate(n - i - 1)) {
+                            ans.add("(" + p + ")" + q);
+                        }
+                    }
                 }
             }
+            cache[n] = ans;
+            return ans;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
