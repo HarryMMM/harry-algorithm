5ea67fe7ee7616cc2c08180a40f3c491697dbb40
haorui.hao <haorui.hao@accenture.com>
2023-08-15 15:40:55 +0800

    feature(P24) solving problem by solution 2

--- src/main/java/com/harryai/algorithm/leetcode/editor/cn/P24SwapNodesInPairs.java
+++ src/main/java/com/harryai/algorithm/leetcode/editor/cn/P24SwapNodesInPairs.java
@@ -53,16 +53,14 @@ public class P24SwapNodesInPairs {
     /**
      * topic: Swap Nodes in Pairs
      * Solution number: 1
-     * Ideas: 链表，因为两表两个节点交换后会需要重新维护3个关联关系，所以使用三指针（PRE,L,R）。
-     * 1. 使用虚拟头节点dummy(next=链表头节点)，将指针PRE指向dummy
-     * 2. 遍历，检查边界，边界有效，L往后移动一步（PRE.NEXT），R移动到L的后继节点(PRE.NEXT.NEXT)，否则结束返回结果
-     * 3. 交换两个节点，并维护三个关联关系（ l.next = r.next;r.next = l;  pre.next = r;）
-     * 4. PRE向后移动到下一组要交换的节点的前序节点（pre=pre.next.next）
-     * 4. 继续2到3步骤，直到结束
-     * Problem boundary: 遍历到链表结束 pre.next != null && pre.next.next != null
-     * Problem pattern: 链表，三指针
+     * Ideas: 链表，递归
+     * 1. 交换第一组两个节点
+     * 2. 交换下一组两个节点
+     * 3. 连接两组节点
+     * Problem boundary: 遍历到链表没有节点或者只有一个节点
+     * Problem pattern: 递归
      * time complexity: O(L) L为链表长度
-     * space complexity: O(1) 不需要额外控件
+     * space complexity: O(L) 不需要额外控件
      */
     //leetcode submit region begin(Prohibit modification and deletion)
 
@@ -78,28 +76,71 @@ public class P24SwapNodesInPairs {
      */
     class Solution {
         public ListNode swapPairs(ListNode head) {
-            ListNode dummy = new ListNode();
-            dummy.next = head;
-            ListNode pre = dummy;
-            ListNode l;
-            ListNode r;
-
-            while (pre.next != null && pre.next.next != null) {
-                // 左右分别向后走一步
-                l = pre.next;
-                r = l.next;
-
-                // 交换
-                l.next = r.next;
-                r.next = l;
-                pre.next = r;
-
-                // PRE节点向后走一步
-                pre = pre.next.next;
+            // 没有节点或者只剩一个节点，不需要交换
+            if (head == null || head.next == null) {
+                return head;
             }
-            return dummy.next;
+
+            // 交换当前组节点
+            ListNode newHead = head.next;
+            ListNode nextGroupHeader = newHead.next;
+            newHead.next = head;
+            // 交换下一组节点并挂到本组节点上
+            head.next = swapPairs(nextGroupHeader);
+            //返回当前组交换后的新head
+            return newHead;
         }
     }
+//    /**
+//     * topic: Swap Nodes in Pairs
+//     * Solution number: 1
+//     * Ideas: 链表，因为两表两个节点交换后会需要重新维护3个关联关系，所以使用三指针（PRE,L,R）。
+//     * 1. 使用虚拟头节点dummy(next=链表头节点)，将指针PRE指向dummy
+//     * 2. 遍历，检查边界，边界有效，L往后移动一步（PRE.NEXT），R移动到L的后继节点(PRE.NEXT.NEXT)，否则结束返回结果
+//     * 3. 交换两个节点，并维护三个关联关系（ l.next = r.next;r.next = l;  pre.next = r;）
+//     * 4. PRE向后移动到下一组要交换的节点的前序节点（pre=pre.next.next）
+//     * 4. 继续2到3步骤，直到结束
+//     * Problem boundary: 遍历到链表结束 pre.next != null && pre.next.next != null
+//     * Problem pattern: 链表，三指针
+//     * time complexity: O(L) L为链表长度
+//     * space complexity: O(1) 不需要额外控件
+//     */
+//    //leetcode submit region begin(Prohibit modification and deletion)
+//
+//    /**
+//     * Definition for singly-linked list.
+//     * public class ListNode {
+//     * int val;
+//     * ListNode next;
+//     * ListNode() {}
+//     * ListNode(int val) { this.val = val; }
+//     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//     * }
+//     */
+//    class Solution {
+//        public ListNode swapPairs(ListNode head) {
+//            ListNode dummy = new ListNode();
+//            dummy.next = head;
+//            ListNode pre = dummy;
+//            ListNode l;
+//            ListNode r;
+//
+//            while (pre.next != null && pre.next.next != null) {
+//                // 左右分别向后走一步
+//                l = pre.next;
+//                r = l.next;
+//
+//                // 交换
+//                l.next = r.next;
+//                r.next = l;
+//                pre.next = r;
+//
+//                // PRE节点向后走一步
+//                pre = pre.next.next;
+//            }
+//            return dummy.next;
+//        }
+//    }
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
